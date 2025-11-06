7ceff57da9b71bd08611c84548fecc2fea62de1e
GhosttBatt <ghosttbatt@gmail.com>
2025-11-06 21:13:40 +0530

    flames.py

--- VIPMUSIC/plugins/admins/flames.py
+++ VIPMUSIC/plugins/admins/flames.py
@@ -153,7 +153,7 @@ def emoji_bar(percent):
 
 
 # --- /FLAMES COMMAND ---
-@Client.on_message(filters.command("flames"))
+@app.on_message(filters.command("flames"))
 async def flames_command(client, message):
     try:
         args = message.text.split(None, 2)
@@ -202,7 +202,7 @@ async def flames_command(client, message):
 
 
 # --- /MATCH COMMAND ---
-@Client.on_message(filters.command("match"))
+@app.on_message(filters.command("match"))
 async def match_command(client, message):
     try:
         if not message.chat.type in ["supergroup", "group"]:
