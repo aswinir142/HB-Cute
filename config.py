64c44adadc9be198d6231cb04755f433ae305311
aswinir142 <aswinir142@gmail.com>
2025-11-19 13:45:13 +0530

    config.py

--- config.py
+++ config.py
@@ -21,7 +21,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://zewdatabase:ijoXgdmQ0NCyg9DO
 GPT_API = getenv("GPT_API", "sk-proj-h6pk40oVRIxpXwrf3i50T3BlbkFJGVET8wX1yJtdi0zCWjDQ")
 PLAYHT_API = getenv("PLAYHT_API", "22e323f342024c0fb4ee430eeb9d0011")
 DATABASE_NAME = getenv("DATABASE_NAME","GhosttBattFed")
-
+#ChatBot
+MONGO_URL = getenv("MONGO_URL","mongodb+srv://iamnobita1:nobitamusic1@cluster0.k08op.mongodb.net/?retryWrites=true&w=majority")
 #Reaction Bot - TRUE on / FALSE off
 REACTION_ENABLED = getenv("REACTION_ENABLED","True")
 
